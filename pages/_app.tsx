d3beed83e2c963ba43c346622ceb485c60f469e3
Colton Pemberton <colton@coltonpemberton.com>
2022-05-30 21:30:42 -0700

    aws config update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../assets/bootswatch.css'
 import type { AppProps } from 'next/app'
 
 import Amplify from 'aws-amplify'
-import awsconfig from '../aws-exports'
+import awsconfig from '../src/aws-exports'
 
 Amplify.configure({ ...awsconfig, ssr: true })
 
