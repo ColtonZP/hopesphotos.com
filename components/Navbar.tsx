1c2a0ec8f895e740867dae6880ab34947e8fbfcd
Colton Pemberton <colton@coltonpemberton.com>
2022-06-04 09:13:24 -0700

    admin page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,13 +5,15 @@ export const NavBar = () => {
   return (
     <Navbar collapseOnSelect expand="sm">
       <Container>
-        <Navbar.Brand href="/">Hope&apos;s Photos</Navbar.Brand>
+        <Navbar.Brand href="https://hopesphotos.com">
+          Hope&apos;s Photos
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
           <Nav>
-            <Nav.Link href="/gallery">Gallery</Nav.Link>
-            <Nav.Link href="">About</Nav.Link>
-            <Nav.Link href="">Contact</Nav.Link>
+            <Nav.Link href="https://hopesphotos.com/gallery">Gallery</Nav.Link>
+            <Nav.Link href="https://hopesphotos.com/">About</Nav.Link>
+            <Nav.Link href="https://hopesphotos.com/">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,106 @@
-import { withSSRContext } from 'aws-amplify'
-
-import type { NextPage } from 'next'
+import React, { useEffect, useState } from 'react'
+import { API, graphqlOperation, Storage, withSSRContext } from 'aws-amplify'
+import { Button, Card, Col, Form, Row } from 'react-bootstrap'
 import Head from 'next/head'
-
-import { Carousel } from 'react-bootstrap'
-
+import { Authenticator } from '@aws-amplify/ui-react'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Observable } from 'zen-observable-ts'
+import { NextPage } from 'next'
 import { AmplifyS3Image } from '@aws-amplify/ui-react-v1'
 import { NavBar } from '../components/Navbar'
-import Footer from '../components/Footer'
+// eslint-disable-next-line import/no-unresolved
+import '@aws-amplify/ui-react/styles.css'
+import { createPhoto, deletePhoto } from '../src/graphql/mutations'
 import { listPhotos } from '../src/graphql/queries'
+import { onCreatePhoto, onDeletePhoto } from '../src/graphql/subscriptions'
+
+type FormData = {
+  photos: {
+    files: FileList
+  }
+  isHomepage: { checked: boolean }
+}
 
 export async function getServerSideProps() {
   const SSR = withSSRContext()
-
-  const { data } = await SSR.API.graphql({
-    query: listPhotos,
-    variables: { filter: { key: { contains: 'homepage/' } } },
-  })
+  const { data } = await SSR.API.graphql({ query: listPhotos })
 
   return {
     props: {
-      photos: data.listPhotos.items,
+      photosQuery: data.listPhotos.items,
     },
   }
 }
 
-const Home: NextPage<{ photos: any }> = ({ photos }) => {
+const Index: NextPage<{ photosQuery: any }> = ({ photosQuery }) => {
+  const [validated] = useState(false)
+  const [photosData, setPhotosData] = useState(photosQuery)
+
+  useEffect(() => {
+    const createPhotoSubscription = (
+      API.graphql(graphqlOperation(onCreatePhoto)) as Observable<any>
+    ).subscribe({
+      next: newPhoto => {
+        setPhotosData(prev => [...prev, newPhoto.value.data.onCreatePhoto])
+      },
+    })
+
+    return () => createPhotoSubscription.unsubscribe()
+  }, [])
+
+  useEffect(() => {
+    const deletePhotoSubscription = (
+      API.graphql(graphqlOperation(onDeletePhoto)) as Observable<any>
+    ).subscribe({
+      next: deletedPhoto => {
+        setPhotosData(prev =>
+          prev.filter(
+            photo => photo.id !== deletedPhoto.value.data.onDeletePhoto.id,
+          ),
+        )
+      },
+    })
+
+    return () => deletePhotoSubscription.unsubscribe()
+  }, [])
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const { photos, isHomepage } = e.target as typeof e.target & FormData
+
+    Object.values(photos.files).forEach(async photo => {
+      const key = `${isHomepage.checked ? 'homepage' : 'gallery'}/${photo.name}`
+
+      await Storage.put(key, photo)
+
+      await API.graphql({
+        query: createPhoto,
+        variables: {
+          input: {
+            key,
+          },
+        },
+      })
+    })
+  }
+
+  const handleDelete = async (photo: any) => {
+    await API.graphql({
+      query: deletePhoto,
+      variables: {
+        input: {
+          id: photo.id,
+        },
+      },
+    })
+
+    await Storage.remove(photo.key)
+  }
+
   return (
-    <div className="vh-100 d-flex flex-column">
+    <>
       <Head>
-        <title>Hope&apos;s Photos</title>
+        <title>Admin</title>
         <meta
           name="description"
           content="Animal photographer based in Seattle, Washington."
@@ -40,61 +111,77 @@ const Home: NextPage<{ photos: any }> = ({ photos }) => {
       <NavBar />
 
       <main className="container flex-grow-1 d-flex flex-column">
-        <h1 className="mb-4 text-center">
-          Photographer based in Seattle, Washington.
-        </h1>
+        <Authenticator hideSignUp>
+          {({ signOut }) => (
+            <>
+              <h1>Admin Panel</h1>
 
-        <Carousel
-          controls={false}
-          indicators={false}
-          fade
-          className="carousel slide flex-grow-1">
-          {!!photos.length &&
-            photos.map(photo => (
-              <Carousel.Item
-                className="h-0"
-                style={{ height: 0, minHeight: '100%' }}
-                key={photo.key}>
-                <AmplifyS3Image
-                  imgKey={photo.key}
-                  alt="dog"
-                  imgProps={{ objectFit: 'cover' }}
-                  className="h-100"
-                />
-              </Carousel.Item>
-            ))}
-        </Carousel>
+              <Form
+                className="my-5"
+                validated={validated}
+                onSubmit={handleSubmit}>
+                <Form.Label>Upload Photos</Form.Label>
+
+                <Form.Group className="mb-3" controlId="isHomepage">
+                  <Form.Check
+                    name="isHomepage"
+                    type="checkbox"
+                    label="Homepage"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="homepage-photos">
+                  <Form.Control
+                    name="photos"
+                    type="file"
+                    accept="image/png, image/jpeg"
+                    className="my-3"
+                    multiple
+                    required
+                  />
+                </Form.Group>
+
+                <Button className="w-100" variant="primary" type="submit">
+                  Upload
+                </Button>
+              </Form>
+
+              <Row xs={1} sm={1} md={6} lg={12} className="mb-5">
+                {photosData.map(photo => (
+                  <Col key={photo.key} className="mb-3">
+                    <Card>
+                      <AmplifyS3Image imgKey={photo.key} alt="dog" />
+                      <Card.Body>
+                        <Card.Title
+                          className={
+                            photo.key.match('homepage')
+                              ? 'text-success'
+                              : 'text-info'
+                          }>
+                          {photo.key.match('homepage') ? 'homepage' : 'gallery'}
+                        </Card.Title>
+                        <Card.Text>{photo.key}</Card.Text>
+                        <Button
+                          variant="danger"
+                          className="w-100"
+                          onClick={() => handleDelete(photo)}>
+                          Delete
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+
+              <Button variant="danger" type="button" onClick={signOut}>
+                Sign out
+              </Button>
+            </>
+          )}
+        </Authenticator>
       </main>
-
-      <Footer />
-    </div>
+    </>
   )
 }
 
-// export async function getServerSideProps() {
-//   const SSR = withSSRContext({ modules: [Storage] })
-//   const photos: { key: string; url: string }[] = []
-//
-//   // const files = Storage.list('homepage/').then(
-//   //   listRes => listRes.filter(file => file.key?.match(/\.(jpg|jpeg|png)$/i)),
-//   //   // .map(file =>
-//   //   //   Storage.get(file.key!).then(fileRes => {
-//   //   //     photos.push({
-//   //   //       key: file.key!,
-//   //   //       url: fileRes,
-//   //   //     })
-//   //   //   }),
-//   //   // ),
-//   // )
-//
-//   // const files = Storage.list('homepage/').then(listRes => listRes)
-//
-//   return {
-//     props: {
-//       photos,
-//       files,
-//     },
-//   }
-// }
-
-export default Home
+export default Index
